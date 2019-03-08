52a7c34fb7dc53537c19bfef8c3ca7ec4c53f0c7
Heathcliff <dennis.nathan@gmail.com>
2019-03-07 20:09:19 -0700

    Added all of the questions.

---
+++ pres-run/src/data/questions.js
@@ -0,0 +1,12 @@
+export const questions = [
+    'The fate of the free world depends on a one on one basketball game. Who do ya got?',
+    'You have to take one of these two home to meet your parents. Who will it be?',
+    'It\'s Spring Break, and you\'re signed up for a drinking contest at Señor Frogs, but your partner is already too drunk to participate. Who\'re you calling in as an alternate?',
+    'An impromptu rap battle breaks out in the parking lot of the 7/11. Who\'s your money on?',
+    'It\'s all you can eat breadsticks night at the Olive Garden, and you want to run them out of bread. Who\'s your partner?',
+    'There\'s a rabbit infestation in the White House Rose Garden. Who do you trust to handle this national crisis?',
+    'It\'s almost time for the company homemade chili contest, and you just pulled a Kevin Malone and spilled yours everywhere. Who do you trust to help you scoop it all up with only the finest Dunder Mifflin paper?',
+    'It\'s time for the Gold Medal figure skating event at the 2022 Winter Olympics, and the United States\' top skater just broke his leg. Who\'s got the triple axel skills to replace them?',
+    'Céline Dion just tripped and injured herself on the way off the stage, and can\'t go back out for her encore. Who do you pick to go out and give their chill-inducing rendition of My Heart Will Go On?',
+    'Two candidates, one deck of cards. Who takes the cake in an all out game of War?'
+]
