ce676d253b030548f85b219a0db7aae5278764d6
Heathcliff <dennis.nathan@gmail.com>
2019-03-07 19:32:50 -0700

    Built array of presidents in data > presidents.

---
+++ pres-run/src/data/presidents.js
@@ -0,0 +1,34 @@
+export const presidents = [
+    {
+        name: 'Francis Underwood',
+        image: 'http://s.wsj.net/public/resources/images/BN-OW386_FAKEPO_M_20160712113511.jpg'
+    },
+    {
+        name: 'Selina Meyer',
+        image: 'http://s.wsj.net/public/resources/images/BN-OW178_0711FK_M_20160711195933.jpg'
+    },
+    {
+        name: 'Marcus Robins',
+        image: 'http://s.wsj.net/public/resources/images/BN-OW176_0711FK_M_20160711195925.jpg'
+    },
+    {
+        name: 'James Sawyer',
+        image: 'http://s.wsj.net/public/resources/images/BN-OV897_0711FK_M_20160711140919.jpg'
+    },
+    {
+        name: 'Jed Bartlet',
+        image: 'http://s.wsj.net/public/resources/images/BN-OV898_0711FK_M_20160711140919.jpg'
+    },
+    {
+        name: 'Thomas Whitmore',
+        image: 'http://s.wsj.net/public/resources/images/BN-OV901_0711FK_M_20160711140920.jpg'
+    },
+    {
+        name: 'James Marshall',
+        image: 'http://s.wsj.net/public/resources/images/BN-OV899_0711FK_M_20160711140920.jpg'
+    },
+    {
+        name: 'Fitzgerald Grant',
+        image: 'http://s.wsj.net/public/resources/images/BN-OW180_0711FK_M_20160711195934.jpg'
+    }
+]
